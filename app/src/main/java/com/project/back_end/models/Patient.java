6ce995f3324cafd2f0424dbb67dee497df8fcb4d
AshikaWijerathna <wijerathnaashika@gmail.com>
2026-05-29 08:53:06 +0530

    Added entity title to the patient model

--- app/src/main/java/com/project/back_end/models/Patient.java
+++ app/src/main/java/com/project/back_end/models/Patient.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
